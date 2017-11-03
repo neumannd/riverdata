9d7d8fe78ea5c9583ca4ad384588e1850453e619
neumannd <daniel.neumann@io-warnemuende.de>
2017-11-03 18:37:55 +0100

    consistency updates

--- R/read.swat.data.rch.2012.R
+++ R/read.swat.data.rch.2012.R
@@ -29,66 +29,110 @@
 #'   
 read.swat.data.rch.2012 <- function(fileHandle, nrow.data, header = TRUE) {
   
-  # str.diag <- rep('', nrow.data)
-  
-  # some constants
-  izero = as.integer(0)
-  rzero = as.numeric(0.0)
-  
   # column indices; first column is the start index and the second column the end index
   ncol = 49
   int.cols = array(c(7,12,21,(1:46)*12+14, 10, 19, 25, (1:46)*12+25), dim = c(ncol,2))
   
   # data.frame for output data:
-  data.out = as.data.frame(list('RCH' = rep(izero, nrow.data) ,
-                                'GIS' = rep(izero, nrow.data) ,
-                                'MON' = rep(izero, nrow.data) ,
-                                'AREA' = rep(rzero, nrow.data) ,
-                                'FLOW_IN' = rep(rzero, nrow.data) ,
-                                'FLOW_OUT' = rep(rzero, nrow.data) ,
-                                'EVAP' = rep(rzero, nrow.data) ,
-                                'TLOSS' = rep(rzero, nrow.data) ,
-                                'SED_IN' = rep(rzero, nrow.data) ,
-                                'SED_OUT' = rep(rzero, nrow.data) ,
-                                'SEDCONC' = rep(rzero, nrow.data) ,
-                                'ORGN_IN' = rep(rzero, nrow.data) ,
-                                'ORGN_OUT' = rep(rzero, nrow.data) ,
-                                'ORGP_IN' = rep(rzero, nrow.data) ,
-                                'ORGP_OUT' = rep(rzero, nrow.data) ,
-                                'NO3_IN' = rep(rzero, nrow.data) ,
-                                'NO3_OUT' = rep(rzero, nrow.data) ,
-                                'NH4_IN' = rep(rzero, nrow.data) ,
-                                'NH4_OUT' = rep(rzero, nrow.data) ,
-                                'NO2_IN' = rep(rzero, nrow.data) ,
-                                'NO2_OUT' = rep(rzero, nrow.data) ,
-                                'MINP_IN' = rep(rzero, nrow.data) ,
-                                'MINP_OUT' = rep(rzero, nrow.data) ,
-                                'CHLA_IN' = rep(rzero, nrow.data) ,
-                                'CHLA_OUT' = rep(rzero, nrow.data) ,
-                                'CBOD_IN' = rep(rzero, nrow.data) ,
-                                'CBOD_OUT' = rep(rzero, nrow.data) ,
-                                'DISOX_IN' = rep(rzero, nrow.data) ,
-                                'DISOX_OUT' = rep(rzero, nrow.data) ,
-                                'SOLPST_IN' = rep(rzero, nrow.data) ,
-                                'SOLPST_OUT' = rep(rzero, nrow.data) ,
-                                'SORPST_IN' = rep(rzero, nrow.data) ,
-                                'SORPST_OUT' = rep(rzero, nrow.data) ,
-                                'REACTPST' = rep(rzero, nrow.data) ,
-                                'VOLPST' = rep(rzero, nrow.data) ,
-                                'SETTLPST' = rep(rzero, nrow.data) ,
-                                'RESUSP_PST' = rep(rzero, nrow.data) ,
-                                'DIFFUSEPST' = rep(rzero, nrow.data) ,
-                                'REACBEDPST' = rep(rzero, nrow.data) ,
-                                'BURYPST' = rep(rzero, nrow.data) ,
-                                'BED_PST' = rep(rzero, nrow.data) ,
-                                'BACTP_OUT' = rep(rzero, nrow.data) ,
-                                'BACTLP_OUT' = rep(rzero, nrow.data) ,
-                                'CMETAL#1' = rep(rzero, nrow.data) ,
-                                'CMETAL#2' = rep(rzero, nrow.data) ,
-                                'CMETAL#3' = rep(rzero, nrow.data) ,
-                                'TOT N' = rep(rzero, nrow.data) ,
-                                'TOT P' = rep(rzero, nrow.data) ,
-                                'NO3CONC' = rep(rzero, nrow.data)))
+  data.out = list()
+  data.out$data = as.data.frame(list('RCH' = rep(0L, nrow.data) ,
+                                     'GIS' = rep(0L, nrow.data) ,
+                                     'MON' = rep(0L, nrow.data) ,
+                                     'AREA' = rep(0.0, nrow.data) ,
+                                     'FLOW_IN' = rep(0.0, nrow.data) ,
+                                     'FLOW_OUT' = rep(0.0, nrow.data) ,
+                                     'EVAP' = rep(0.0, nrow.data) ,
+                                     'TLOSS' = rep(0.0, nrow.data) ,
+                                     'SED_IN' = rep(0.0, nrow.data) ,
+                                     'SED_OUT' = rep(0.0, nrow.data) ,
+                                     'SEDCONC' = rep(0.0, nrow.data) ,
+                                     'ORGN_IN' = rep(0.0, nrow.data) ,
+                                     'ORGN_OUT' = rep(0.0, nrow.data) ,
+                                     'ORGP_IN' = rep(0.0, nrow.data) ,
+                                     'ORGP_OUT' = rep(0.0, nrow.data) ,
+                                     'NO3_IN' = rep(0.0, nrow.data) ,
+                                     'NO3_OUT' = rep(0.0, nrow.data) ,
+                                     'NH4_IN' = rep(0.0, nrow.data) ,
+                                     'NH4_OUT' = rep(0.0, nrow.data) ,
+                                     'NO2_IN' = rep(0.0, nrow.data) ,
+                                     'NO2_OUT' = rep(0.0, nrow.data) ,
+                                     'MINP_IN' = rep(0.0, nrow.data) ,
+                                     'MINP_OUT' = rep(0.0, nrow.data) ,
+                                     'CHLA_IN' = rep(0.0, nrow.data) ,
+                                     'CHLA_OUT' = rep(0.0, nrow.data) ,
+                                     'CBOD_IN' = rep(0.0, nrow.data) ,
+                                     'CBOD_OUT' = rep(0.0, nrow.data) ,
+                                     'DISOX_IN' = rep(0.0, nrow.data) ,
+                                     'DISOX_OUT' = rep(0.0, nrow.data) ,
+                                     'SOLPST_IN' = rep(0.0, nrow.data) ,
+                                     'SOLPST_OUT' = rep(0.0, nrow.data) ,
+                                     'SORPST_IN' = rep(0.0, nrow.data) ,
+                                     'SORPST_OUT' = rep(0.0, nrow.data) ,
+                                     'REACTPST' = rep(0.0, nrow.data) ,
+                                     'VOLPST' = rep(0.0, nrow.data) ,
+                                     'SETTLPST' = rep(0.0, nrow.data) ,
+                                     'RESUSP_PST' = rep(0.0, nrow.data) ,
+                                     'DIFFUSEPST' = rep(0.0, nrow.data) ,
+                                     'REACBEDPST' = rep(0.0, nrow.data) ,
+                                     'BURYPST' = rep(0.0, nrow.data) ,
+                                     'BED_PST' = rep(0.0, nrow.data) ,
+                                     'BACTP_OUT' = rep(0.0, nrow.data) ,
+                                     'BACTLP_OUT' = rep(0.0, nrow.data) ,
+                                     'CMETAL#1' = rep(0.0, nrow.data) ,
+                                     'CMETAL#2' = rep(0.0, nrow.data) ,
+                                     'CMETAL#3' = rep(0.0, nrow.data) ,
+                                     'TOT N' = rep(0.0, nrow.data) ,
+                                     'TOT P' = rep(0.0, nrow.data) ,
+                                     'NO3CONC' = rep(0.0, nrow.data)))
+  data.out$units <- c('',
+                      '',
+                      '',
+                      'km2',
+                      'm3/s',
+                      'm3/s',
+                      'm3/s',
+                      'm3/s',
+                      't',
+                      't',
+                      'mg/L',
+                      'kg N',
+                      'kg N',
+                      'kg P',
+                      'kg P',
+                      'kg N',
+                      'kg N',
+                      'kg N',
+                      'kg N',
+                      'kg N',
+                      'kg N',
+                      'kg P',
+                      'kg P',
+                      'kg chl-a',
+                      'kg chl-a',
+                      'kg O2',
+                      'kg O2',
+                      'kg O2',
+                      'kg O2',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      'mg active ingredient',
+                      '#cfu/100 mL',
+                      '#cfu/100 mL',
+                      'kg',
+                      'kg',
+                      'kg',
+                      'kg N',
+                      'kg P',
+                      'mg NO3-N/kg sed')
   
   # expected default header
   str.header.dflt <- '       RCH      GIS   MON     AREAkm2  FLOW_INcms FLOW_OUTcms     EVAPcms    TLOSScms  SED_INtons SED_OUTtons SEDCONCmg/L   ORGN_INkg  ORGN_OUTkg   ORGP_INkg  ORGP_OUTkg    NO3_INkg   NO3_OUTkg    NH4_INkg   NH4_OUTkg    NO2_INkg   NO2_OUTkg   MINP_INkg  MINP_OUTkg   CHLA_INkg  CHLA_OUTkg   CBOD_INkg  CBOD_OUTkg  DISOX_INkg DISOX_OUTkg SOLPST_INmgSOLPST_OUTmg SORPST_INmgSORPST_OUTmg  REACTPSTmg    VOLPSTmg  SETTLPSTmgRESUSP_PSTmgDIFFUSEPSTmgREACBEDPSTmg   BURYPSTmg   BED_PSTmg BACTP_OUTctBACTLP_OUTct  CMETAL#1kg  CMETAL#2kg  CMETAL#3kg     TOT Nkg     TOT Pkg NO3ConcMg/l    WTMPdegc'
@@ -107,7 +151,7 @@ read.swat.data.rch.2012 <- function(fileHandle, nrow.data, header = TRUE) {
       
       # copy new header
       for (icol in 1:ncol) {
-        names(data.out)[icol] = trimws(substr(str.header.real, int.cols[icol, 1], int.cols[icol, 2]))
+        names(data.out$data)[icol] = trimws(substr(str.header.real, int.cols[icol, 1], int.cols[icol, 2]))
       }
     }
   }
@@ -116,19 +160,18 @@ read.swat.data.rch.2012 <- function(fileHandle, nrow.data, header = TRUE) {
   # read the data
   for (irow in 1:nrow.data) {
     str.tmp <- readLines(fileHandle, n = 1L)
-    # str.diag[irow] = str.tmp
     
     # copy integers (first three columns)
     for (icol in 1:3) {
-      data.out[irow, icol] = strtoi(substr(str.tmp, int.cols[icol, 1], int.cols[icol, 2]))
+      data.out$data[irow, icol] = strtoi(substr(str.tmp, int.cols[icol, 1], int.cols[icol, 2]))
     }
     
     # copy numerics/floats/doubles
     for (icol in 4:ncol) {
-      data.out[irow, icol] = as.numeric(substr(str.tmp, int.cols[icol, 1], int.cols[icol, 2]))
+      data.out$data[irow, icol] = as.numeric(substr(str.tmp, int.cols[icol, 1], int.cols[icol, 2]))
     }
   }
   
-  # return(list('df'=data.out, 'diag'=str.diag))
+  # return output
   return(data.out)
 }
